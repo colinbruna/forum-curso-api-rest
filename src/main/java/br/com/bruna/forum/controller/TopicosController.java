9340e351a201135ea80d47eec9c504d54d2b13f1
colinbruna <brunacbaldin@gmail.com>
2022-06-30 15:41:17 -0300

    funcionalidade detalhar topico

--- src/main/java/br/com/bruna/forum/controller/TopicosController.java
+++ src/main/java/br/com/bruna/forum/controller/TopicosController.java
@@ -1,5 +1,6 @@
 package br.com.bruna.forum.controller;
 
+import br.com.bruna.forum.controller.dto.DetalhesDoTopicoDTO;
 import br.com.bruna.forum.controller.dto.TopicoDTO;
 import br.com.bruna.forum.controller.form.TopicoForm;
 import br.com.bruna.forum.modelo.Topico;
@@ -42,5 +43,11 @@ public class TopicosController {
 
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+    }
+
+    @GetMapping("/{id}")
+    public DetalhesDoTopicoDTO detalhar(@PathVariable Long id) {
+        Topico topico = topicoRepository.getReferenceById(id);
+        return new DetalhesDoTopicoDTO(topico);
     }
 }
