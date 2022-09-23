f8954665f43c934ca6e6c141ade9e2d97ee3a135
colinbruna <brunacbaldin@gmail.com>
2022-09-23 14:53:04 -0300

    implementacao spring security

---
+++ src/main/java/br/com/bruna/forum/config/security/AutenticacaoService.java
@@ -0,0 +1,31 @@
+package br.com.bruna.forum.config.security;
+
+import br.com.bruna.forum.modelo.Usuario;
+import br.com.bruna.forum.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(final UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> usuario = usuarioRepository.findByEmail(username);
+
+        if (usuario.isPresent()) {
+            return usuario.get();
+        }
+
+        throw new UsernameNotFoundException("Dados inválidos.");
+    }
+}
