9b4a7f1f61cb623e53776455180592ccdebd8865
colinbruna <brunacbaldin@gmail.com>
2022-09-23 16:16:15 -0300

    gerando token com JWT

--- src/main/java/br/com/bruna/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/bruna/forum/config/security/SecurityConfigurations.java
@@ -1,12 +1,15 @@
 package br.com.bruna.forum.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -17,6 +20,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     public SecurityConfigurations(AutenticacaoService autenticacaoService) {
         this.autenticacaoService = autenticacaoService;
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     //configurações de autenticação
@@ -31,8 +40,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin();
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     //configurações de recursos estáticos(js, css, imagens, etc.)
